ce85ba0e7c82c7b9b234ffaf2fbac4f774e5f22a
Prashant <contactprsant@gmail.com>
2023-08-23 13:41:23 +0530

    UI Update

--- src/App.jsx
+++ src/App.jsx
@@ -26,16 +26,11 @@ const App = () => {
   return (
     <div className="container">
 
-<<<<<<< HEAD
-      <p className='main-heading'> {!word ? `Words Web` : word}</p>
+      <p className='main-heading'> {!word ? `Words-web` : word}</p>
 
-=======
-      <p style={{fontWeight:'600',fontSize:'4rem', textAlign : 'center'}}> { !word?`Words Web`:word }</p>
-     
->>>>>>> 894dce8daefa3f536205c55638b4ac77d617d1d3
       <Input setWord={setWord} />
 
-  { meanings ? <Result meanings={meanings} word={word} /> : <span className='emptyResult results'>Search Something...</span> }
+      {meanings ? <Result meanings={meanings} word={word} /> : <span className='emptyResult results'>Search Something...</span>}
 
     </div >
   )
